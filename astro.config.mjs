59276317560cb888337b0d37cdc52d8c75f06e68
Rio Sugiarno <riosugiarno@192.168.1.9>
2026-07-28 14:08:18 +0700

    Add Preact integration, Greeting component, and hydrate island with client:load

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://your-netflify-url.netlify.app"
+  site: "https://your-netflify-url.netlify.app",
+  integrations: [preact()]
 });
